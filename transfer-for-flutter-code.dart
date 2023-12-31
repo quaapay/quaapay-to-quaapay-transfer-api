f1fd138143ca7765d4ff91c27991c88873a2e110
Team Quaapay <120090402+quaapay@users.noreply.github.com>
2023-12-31 13:01:24 +0100

    Update transfer-for-flutter-code.dart

--- transfer-for-flutter-code.dart
+++ transfer-for-flutter-code.dart
@@ -36,7 +36,7 @@ void main() async {
     "password": password,
     "amount": amount,
     "id": walletIds,
-    "receiver": "quaaapay@gmail.com",
+    "receiver": receiver,
     "message": "here is money for my sisters wedding and preparations take good care of everything. Thank you"
   };
 
